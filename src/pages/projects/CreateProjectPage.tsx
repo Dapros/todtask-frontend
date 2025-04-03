b86625070e0339762051d2bd52a0d77e940a2167
Sergio R <sergiodrp70@gmail.com>
2025-04-03 13:16:01 -0500

    save changes

--- src/pages/projects/CreateProjectPage.tsx
+++ src/pages/projects/CreateProjectPage.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useForm } from 'react-hook-form'
 import ProjectForm from "@/components/projects/ProjectForm";
 import { ProjectFormData } from "@/types/index";
@@ -6,6 +6,8 @@ import { createProject } from "@/api/ProjectAPI";
 
 
 export default function CreateProjectPage() {
+
+  const navigate = useNavigate()
 
   const initialValues : ProjectFormData = {
     projectName: "",
@@ -15,8 +17,9 @@ export default function CreateProjectPage() {
 
   const { register, handleSubmit, formState: {errors} } = useForm({defaultValues: {initialValues}})
 
-  const handleForm = (data : ProjectFormData) => {
-    createProject(data)
+  const handleForm = async (data : ProjectFormData) => {
+    await createProject(data)
+    navigate('/')
   }
 
   return (
