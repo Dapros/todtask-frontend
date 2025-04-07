0929b7450c1433d0cf20473be3460d44409d2b0d
Sergio R <sergiodrp70@gmail.com>
2025-04-07 12:53:30 -0500

    first add task in the project

---
+++ src/pages/projects/ProjectDetailsView.tsx
@@ -0,0 +1,35 @@
+import { getProjectById } from "@/api/ProjectAPI"
+import AddTaskModal from "@/components/task/AddTaskModal"
+import { useQuery } from "@tanstack/react-query"
+import { Navigate, useNavigate, useParams } from "react-router-dom"
+
+export default function ProjectDetailsView() {
+
+  const navigate = useNavigate()
+
+  const params = useParams() // extraemos el parametro en este caso sera projectId
+  const projectId = params.projectId! // projectId con ! para que no infiera undefined
+  const { data, isLoading, isError} = useQuery({ // query
+    queryKey: ['editProject', projectId],
+    queryFn: () => getProjectById(projectId),
+    retry: false
+  })
+  if(isLoading) return 'Cargando...'
+  if(isError) return <Navigate to='/404' />
+  if(data) return (
+    <>
+     <h1 className="text-5xl font-black">{data.projectName}</h1> 
+     <p className="text-2xl font-light text-gray-500 mt-5">{data.description}</p>
+
+     <nav className="my-5 flex gap-3">
+      <button
+        type="button"
+        className="bg-teal-400 hover:bg-teal-500 px-10 py-3 text-white  text-xl font-bold cursor-pointer transition-colors"
+        onClick={() => navigate('?newTask=true')}
+      >Agregar Tarea</button>
+     </nav>
+
+     <AddTaskModal />
+    </>
+  )
+}
--- src/router.tsx
+++ src/router.tsx
@@ -2,7 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import AppLayout from './layouts/AppLayout'
 import DashboardPage from '@/pages/DashboardPage'
 import CreateProjectPage from './pages/projects/CreateProjectPage'
-import EditProjectPage from './pages/projects/EditProjectPAge'
+import EditProjectPage from './pages/projects/EditProjectPage'
+import ProjectDetailsView from './pages/projects/ProjectDetailsView'
 
 export default function Router() {
   return (
@@ -11,6 +12,7 @@ export default function Router() {
         <Route element={<AppLayout />}>
           <Route path='/' element={<DashboardPage />} index />
           <Route path='/projects/create' element={<CreateProjectPage />} />
+          <Route path='/projects/:projectId' element={<ProjectDetailsView />} />
           <Route path='/projects/:projectId/edit' element={<EditProjectPage />} />
 
         </Route>
